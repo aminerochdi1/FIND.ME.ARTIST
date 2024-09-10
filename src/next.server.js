c3001e8335e3004efe3a3af1d51294a2b272d685
aminerochdi <>
2024-09-10 16:50:11 +0100

    changed in next.server.js env from prod to dev

--- src/next.server.js
+++ src/next.server.js
@@ -4,7 +4,8 @@ const http = require('http');
 const fs = require('fs');
 const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production';
+// const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'development';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
